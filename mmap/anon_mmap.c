55f27ab516c23518f671264a12cfbbb507f52c10
teirm <tribunus.vii@gmail.com>
2019-07-05 11:03:38 -0400

    Read chapter 49

---
+++ mmap/anon_mmap.c
@@ -0,0 +1,69 @@
+/*
+ * anon_mmap.c
+ *
+ * Provided by TLPI
+ */
+
+#ifdef USE_MAP_ANON
+#define _BSD_SOURCE
+#endif /* USE_MAP_ANON */
+
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+#include <lib/tlpi_hdr.h>
+
+int
+main(int argc, char *argv[])
+{
+    int *addr;
+
+#ifdef USE_MAP_ANON
+    addr = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE,
+                MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    if (addr == MAP_FAILED) {
+        errExit("mmap");
+    }
+#else /* map /dev/zero */
+    int fd;
+
+    fd = open("/dev/zero", O_RDWR);
+    if (fd == -1) {
+        errExit("open");
+    }
+
+    addr = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (addr == MAP_FAILED) {
+        errExit("mmap");
+    }
+
+    if (close(fd) == -1) {
+        errExit("close");
+    }
+#endif /* USE_MAP_ANON */
+
+    *addr = 1;
+    
+    switch (fork()) {
+        case -1:
+            errExit("fork");
+        case 0:
+            printf("Child started, value = %d\n", *addr);
+            (*addr)++;
+            if (munmap(addr, sizeof(int)) == -1) {
+                errExit("munmap");
+            }
+            exit(EXIT_SUCCESS);
+        default:
+            if (wait(NULL) == -1) {
+                errExit("wait");
+            }
+            printf("In parent, value = %d\n", *addr);
+
+            if (munmap(addr, sizeof(int)) == -1) {
+                errExit("munmap");
+            }
+            exit(EXIT_SUCCESS);
+    }
+}
