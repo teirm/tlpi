44804169d197753cb9ca53858836f6cc26029516
teirm <tribunus.vii@gmail.com>
2020-11-15 14:10:43 -0500

    Moving to CMake instead of manually running compiler

--- fileio/dup_implementations.c
+++ fileio/dup_implementations.c
@@ -36,3 +36,30 @@ my_dup2(int oldfd, int newfd)
     
     return fcntl(oldfd, F_DUPFD, newfd);
 }
+
+int
+main()
+{
+    int fd = open("test_file.txt", O_CREAT);
+    if (fd == -1) {
+        errExit("failed to open file\n");
+    }
+    
+    int new_fd = my_dup(fd);
+    if (new_fd == -1) {
+        errExit("failed my_dup\n");
+    }
+    close(new_fd);
+
+    new_fd = my_dup2(new_fd, 1000);
+    if (new_fd == -1) {
+        errExit("failed my_dup2\n");
+    }
+    
+    close(new_fd);
+    close(fd);
+
+
+    return 0;
+}
+
